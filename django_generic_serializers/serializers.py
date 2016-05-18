7c7d3484d67469b1b5851092091581788e6b6e10
Abhishek K <abhishek.k6006@gmail.com>
2016-05-19 02:36:41 +0530

    Added PyPi reqs

---
+++ django_generic_serializers/serializers.py
@@ -0,0 +1,81 @@
+'''
+#################################################
+# Django Serilizer is a generic serializer for  #
+# for django models objects.                    #
+# --------------------------------------------- #
+# @Authore : Abhishek K                         #
+# @Created on : 18-May-2016                     #
+#                                               #
+#################################################
+'''
+
+
+from django.db.models.fields.related import *
+from django.db.models.fields import DateTimeField
+from django.db.models.query import QuerySet
+from datetime import date, datetime
+#example
+#fields = {
+#    'multi_level' : [org.orgId', 'user.username', 'org.name'] ,
+#    'single': ['happay_user_id']
+#}
+
+class Serializers(object):
+    ''''''
+    def __init__(self):
+        pass
+
+    def is_date_or_datetime_instance(self, data):
+        '''
+            check if the given data is an instance of date or datetime if so
+            then converts it into str
+        '''
+        if isinstance(data,datetime) or isinstance(data,date):
+            data = str(data)
+        return data
+
+    def get_fk_data(self, primary_obj, field_name):
+        '''
+            This Function is used to get data from related field from a models
+            basically ForeignKey and OneToOne Fields
+        '''
+        ''' Splits the given string to sparate objects and then pops the field name '''
+        obj_list = field_name.split('.')
+        field_name = obj_list.pop()
+        obj = primary_obj
+        for obj_str in obj_list:
+            obj = getattr(obj, obj_str)
+        data = getattr(obj,field_name)
+        data = self.is_date_or_datetime_instance(data)
+        return  data #obj._meta.get_field_by_name(field_name)[0].verbose_name
+
+    def get_m2m_data(self, primary_obj,query_data):
+        data = list()
+        query_set = getattr(primary_obj, query_data.get('field_name'))
+        query_set = query_set.all()
+        print
+        if len(query_set) > 0:
+            for query_set_obj in query_set:
+                data.append(self.serializers(query_set_obj, query_data.get('fields')))
+            return data
+        else:
+            return list()
+
+    def serializers(self, obj, fields=None):
+        data_res = dict()
+        fields_type_objs = obj._meta.get_concrete_fields_with_model()
+        fk_field_names = [ field[0].__dict__.get('name') for field in fields_type_objs if isinstance(field[0], ForeignKey) ]
+        m2m_field_names = [ m2m_field.field.name for m2m_field in obj._meta.get_all_related_many_to_many_objects() ]
+
+        for alias, field in fields.iteritems():
+            if isinstance(field, dict):
+                #if field.get('field_name') in m2m_field_names:
+                data_res[alias] = self.get_m2m_data(obj, field)
+            elif len(field.split('.')) > 1:
+                #if field[0] in fk_field_names:
+                data_res[alias] = self.get_fk_data(obj, field)
+            else:
+                data_res[alias] = self.is_date_or_datetime_instance(getattr(obj, field))
+
+        return data_res
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'django_generic_serializers',
+  packages = ['django_generic_serializers'], # this must be the same as the name above
+  version = '0.1',
+  description = 'This serializes and given a object according to the dict passed to it',
+  author = 'Abhishek K gowda',
+  author_email = 'abhishek.k6006@gmail.com',
+  url = 'https://github.com/abhishek246/Django_Serilizer.git', # use the URL to the github repo
+  download_url = 'https://github.com/abhishek246/Django_Serilizer.git/tarball/0.1', # I'll explain this in a second
+  keywords = ['django', 'serializer', 'django-serializers'], # arbitrary keywords
+  classifiers = [],
+)
